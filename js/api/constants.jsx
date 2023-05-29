7e100b617fb37fa2b936e444385104036492427d
Wojciech Pająk <arachnomorph@gmail.com>
2023-05-29 15:20:36 +0200

    updated api key

--- js/api/constants.jsx
+++ js/api/constants.jsx
@@ -1,4 +1,4 @@
-const API_KEY = "e08af436-f63e-4b57-a517-38539de9b7b2";
+const API_KEY = "3d4b1a79-d1ba-410c-9905-e1029b2e103e";
 const API_URL = "https://todo-api.coderslab.pl/api";
 
 export { API_KEY, API_URL };
